f7398cb4c1594cc12f9d3a18435d441809f58dd5
isagrt <isadoragoedert05@gmail.com>
2025-07-16 11:02:11 -0300

    criando a class pai Livros

---
+++ Livros.java
@@ -0,0 +1,23 @@
+public class Livros {
+    //Atributos
+    public int codigo;
+    public String titulo;
+    public String autor;
+    public String editora;
+    public int nmrPaginas;
+    public int edicao;
+    
+
+    //metodos
+    public void Ler(){
+        System.out.println("Estou lendo a biografia da Rita lee");
+    }
+
+    public void Cadastrar(){
+        System.out.println("Estou cadastrando...");
+    }
+
+    public void Localizar(){
+        System.out.println("Estou procurando...");
+    }
+}
