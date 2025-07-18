5790aceab2ecb4eca4764ccf584780b1b0a5e4fa
isagrt <isadoragoedert05@gmail.com>
2025-07-18 11:59:29 -0300

    ok

--- LivroDeLivraria.java
+++ LivroDeLivraria.java
@@ -31,7 +31,6 @@ public class LivroDeLivraria extends Livros {
     public String getCategoria() {
         return categoria;
     }
-
     public void setCategoria(String categoria) {
         this.categoria = categoria;
     }
